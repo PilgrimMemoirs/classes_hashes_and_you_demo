cab9ae607a17b6cd1f0dacf17f06ea7fa27e38d8
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-08-16 07:51:36 -0700

    starter code - hash as argument

---
+++ hashes_classes_and_you_demo.rb
@@ -0,0 +1,27 @@
+#### HASHES, CLASSES & YOU ####
+
+### Hash review:
+  #This is hash:  {name: "ADA Developers Academy", location: "Seattle", area_code: 98161, students: 48 }
+
+  #A hash has key value pairs. Often the keys are symbols, values can be any type
+
+
+
+### Let's setup an Address class
+
+
+
+
+
+
+
+### Let's refactor the Address class to accept a hash as parameters instead
+
+
+
+
+
+
+
+
+### Why do it this way?
