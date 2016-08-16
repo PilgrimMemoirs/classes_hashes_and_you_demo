062ef0789e96ae4242e987464b0a17459fdde58d
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-08-16 08:42:00 -0700

    added hash review

--- hashes_classes_and_you_demo.rb
+++ hashes_classes_and_you_demo.rb
@@ -1,13 +1,36 @@
 #### HASHES, CLASSES & YOU ####
 
 ### Hash review:
-  #This is hash:  {name: "ADA Developers Academy", location: "Seattle", area_code: 98161, students: 48 }
+people = [
+  { name: "Jamie", email: "jamie@adadevelopersacademy.org" },
+  { name: "Chris", email: "chris@adadevelopersacademy.org" }
+]
 
   #A hash has key value pairs. Often the keys are symbols, values can be any type
+jamie = { jamie: # this is the key
+              # this entire hash is the value
+              { last_name: "Pilgrim",
+                first_name: "Jamie",
+                address: {
+                street: "123 fake st",
+                city: "Seattle",
+                state: "WA"
+                },
+                pets: [
+                  { name:    "Amper",
+                    species: "tabby cat"
+                  },
+                  { name:    "Octo",
+                    species: "mini puma"
+                  }
+                ]
+              }
+            }
 
-
+            #How do I find the name's of Jamie's pets? 
 
 ### Let's setup an Address class
+
 
 
 
@@ -25,3 +48,8 @@
 
 
 ### Why do it this way?
+
+
+
+
+### More Practice: Setup a person class that also takes in a hash argument. That hash argument should have at least one address that is an Address object.
